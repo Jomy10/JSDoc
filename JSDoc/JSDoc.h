9179206371a73ebe2386cf1afbbfda2ba37b3fe5
Jomy10 <jonas.vbs4@gmail.com>
2021-09-20 17:50:24 +0200

    Initial project setup

---
+++ JSDoc/JSDoc.h
@@ -0,0 +1,18 @@
+//
+//  JSDoc.h
+//  JSDoc
+//
+//  Created by Jonas Everaert on 20/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JSDoc.
+FOUNDATION_EXPORT double JSDocVersionNumber;
+
+//! Project version string for JSDoc.
+FOUNDATION_EXPORT const unsigned char JSDocVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSDoc/PublicHeader.h>
+
+
